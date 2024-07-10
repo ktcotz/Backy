bc3d3bfd1afcb77cdb442dfa36be720e0ece9e25
ktcotz <naskret.kamil@gmail.com>
2024-07-10 20:44:09 +0200

    feat: bcrypt
    Hashing user password, nauka na temat JWT.

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,12 +1,14 @@
 import { ConflictException, Injectable } from '@nestjs/common';
 import { SignupDTO } from '../dtos/auth.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcryptjs';
+import { UserType } from '@prisma/client';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async signup({ email }: SignupDTO) {
+  async signup({ email, password, name, phone }: SignupDTO) {
     const userExists = await this.prisma.user.findUnique({
       where: {
         email,
@@ -17,6 +19,18 @@ export class AuthService {
       throw new ConflictException();
     }
 
-    return { userExists };
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await this.prisma.user.create({
+      data: {
+        email,
+        name,
+        phone,
+        password: hashedPassword,
+        user_type: UserType.BUYER,
+      },
+    });
+
+    return user;
   }
 }
